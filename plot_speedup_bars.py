1aebb2bf1423cb7cdeb0f82c89950a7c100c0f6d
En Zhou <zhouen.nathan@gmail.com>
2016-11-06 01:22:38 +0200

    Added plot for speedup bars

--- plot_speedup_bars.py
+++ plot_speedup_bars.py
@@ -2,6 +2,7 @@
 import numpy as np
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
+import pandas as pd
 import sys
 from os import listdir
 import os
@@ -31,102 +32,58 @@ for filename in filenames:
 
     print 'Start plotting: {}'.format(filename)
 
-    with open(filename) as f:
-      raw_data = f.read()
-    raw_data = raw_data.split('\n')
+    df = pd.read_csv(filename)
+    df.head()
 
-    print 'Raw Data: \n{}'.format(raw_data)
+    print 'Raw Data: \n{}'.format(df)
 
     plot_data = []
-    for row in raw_data:
+    for row in df:
         if row and not row[0].startswith('#'):
           plot_data.append(row)
-
     print 'Data: \n{}'.format(plot_data)
-    plt_data = {}
-    for d in plot_data:
-        key = d.split(' ')[0]
-        values = []
-        if key != 'Matrix':
-          for v in d.split(' ')[1:]:
-            values.append(float(v))
-        else:
-          for v in d.split(' ')[1:]:
-            values.append(v)
-        plt_data.update(
-          {
-            key: values
-          }
-        )
 
-    # import collections
-    # plt_data = collections.OrderedDict(sorted(plt_data.items()))
-
-    print len(plt_data)
+    print len(plot_data)
     fig, ax = plt.subplots()
-    n_groups = len(plt_data['Matrix'])
+    n_groups = len(plot_data) - 1
     print 'n_groups = {}'.format(n_groups)
     index = np.arange(n_groups)
     print 'index = {}'.format(index)
-    bar_width = 0.2
+    bar_width = 0.15
 
-    # opacity = 0.4
-    # error_config = {'ecolor': '0.3'}
+    ymax = 25
+    ymajor_ticks = np.arange(0, ymax, 2)
+    yminor_ticks = np.arange(0, ymax, 1.0)
 
-    print plt_data
+    print plot_data
     # sys.exit()
 
-    colors = ['r', 'b', 'c', 'y', 'g']
+    colors = ['r', 'b', 'c', 'y', 'g', 'm', 'k']
     offset = 0
-    for key in plt_data.keys():
-    	if key != 'Matrix':
-    		value = plt_data[key]
+    for key, value in df.iteritems():
+    	if key != 'Mesh_Sizes':
+    		value = value.values
     		print value
     		result = plt.bar(index+bar_width*offset, value, bar_width,
                      # alpha=opacity,
                      color=colors[offset],
                      # yerr=std_men,
                      # error_kw=error_config,
-                     label=key.replace('-', ' '))
+                     label=key)
     		offset += 1
 
-    # value = (0.93, 0.58, 0.88)
-    # rects1 = plt.bar(index+bar_width*0, value, bar_width,
-    #                  # alpha=opacity,
-    #                  color='b',
-    #                  # yerr=std_men,
-    #                  # error_kw=error_config,
-    #                  label='GmresDiag_d')
-    # value = (4.64, 2.91, 4.64)
-    # rects2 = plt.bar(index+bar_width*1.0, value, bar_width,
-    #                  # alpha=opacity,
-    #                  color='r',
-    #                  # yerr=std_men,
-    #                  # error_kw=error_config,
-    #                  label='CgDiag_d')
-
-    # value = (5.00, 2.00, 4.00)
-    # rects2 = plt.bar(index+bar_width*2.0, value, bar_width,
-    #                  # alpha=opacity,
-    #                  color='y',
-    #                  # yerr=std_men,
-    #                  # error_kw=error_config,
-    #                  label='Test_d')
-
     # set y-axis range
-    plt.ylim(0, 35)
-    ax.grid(b=True, which='major')
-    ax.grid(b=True, which='minor')                                                      
+    plt.ylim(0, ymax)
     plt.legend(prop={'size':12}, loc='upper left')
-    plt.xlabel('Number of Nodes')
+    plt.xlabel('Mesh Sizes')
     plt.ylabel('Speedup')
     # plt.title(filename.replace('_', ' ').replace('results.dat', '').title())
-    plt.xticks(index + bar_width, plt_data['Matrix'])
-    ax.grid(b=True, which='major')
-    ax.grid(b=True, which='minor')
+    plt.xticks(index + bar_width, df.Mesh_Sizes)
+    ax.set_yticks(ymajor_ticks)
+    ax.set_yticks(yminor_ticks, minor=True)
 
 
     plt.tight_layout()
-    plt.savefig(filename.replace('.dat', '.eps'), format='eps')
-    plt.savefig(filename.replace('.dat', '.png'), format='png')
+    plt.savefig(filename.replace('.csv', '.eps'), format='eps')
+    plt.savefig(filename.replace('.csv', '.png'), format='png')
     plt.show()
