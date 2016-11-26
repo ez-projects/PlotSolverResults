c6088ed0a1bdfb0b00234f1a17c38ccccf74d078
En Zhou <zhouen.nathan@gmail.com>
2016-11-27 01:58:58 +0200

    Added total solver time plot

---
+++ constants.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+STYLES = ['^', 'o', '>', 'v',  'H', '<', 'd', 's', 'p','3', 'D',  'x','+', '|', '.',  '*']
+SOLVERS = ['AmgClassCPU', 'AmgFreeCPU', 'CgDiagCPU', 'CgLusgsCPU', 'GmresDiagCPU', 'GmresLusgsCPU', 'CgDiagGPU','GmresDiagGPU']
