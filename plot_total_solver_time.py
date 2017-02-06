db0ac6887e84ae29d751a258e3212c1f4f20ee43
En Zhou <zhouen.nathan@gmail.com>
2017-02-07 00:07:11 +0200

    Updated total solver plots with 2 decimal places for y-axis Update x-axis label to Mesh Size

--- plot_total_solver_time.py
+++ plot_total_solver_time.py
@@ -57,13 +57,13 @@ for filename in filenames:
     xmax = 1200000
     # maxx = float(df.Timesteps.values[-1])
     # X-Axis is mesh sizes, i.e. from 0 to 1,200,000
-    # xmajor_ticks = np.arange(0, 1200000, 100000)
-    # xminor_ticks = np.arange(0, 1200000, 50000)
+    # xmajor_ticks = np.arange(0, xmax, 100000)
+    # xminor_ticks = np.arange(0, xmax, 50000)
 
     
     # yinterval = round(float(maxy/num_intervals), 2)
     # Y-Axis is the total solve time in hours, i.e. from 0 to 100
-    ymax = 85
+    ymax = 100
     ymajor_ticks = np.arange(0, ymax, 10)
     yminor_ticks = np.arange(0, ymax, 5)
     
@@ -74,9 +74,9 @@ for filename in filenames:
 
     # Set y-tickets to be 2 decimal places
     # http://stackoverflow.com/questions/12608788/changing-the-tick-frequency-on-x-or-y-axis-in-matplotlib
-    # ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.2f'))
+    ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.2f'))
     label_font = 12
-    ax.set_xlabel('Mesh Sizes', fontsize=label_font)
+    ax.set_xlabel('Mesh Size', fontsize=label_font)
     
     # ylabel = filename.replace('_', ' ').replace('.csv', '').title()
 
