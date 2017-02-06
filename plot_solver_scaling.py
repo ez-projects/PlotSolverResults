8e7cca874f16aa4dde0c9bb3ef22e22c52339270
En Zhou <zhouen.nathan@gmail.com>
2017-02-06 23:48:07 +0200

    Plotted solver scaling

--- plot_solver_scaling.py
+++ plot_solver_scaling.py
@@ -63,9 +63,9 @@ for filename in filenames:
     
     # yinterval = round(float(maxy/num_intervals), 2)
     # Y-Axis is the total solve time in hours, i.e. from 0 to 100
-    ymax = 35000
-    ymajor_ticks = np.arange(0, ymax, 2000)
-    yminor_ticks = np.arange(0, ymax, 1000)
+    ymax = 36000
+    ymajor_ticks = np.arange(0, ymax, 3600)
+    yminor_ticks = np.arange(0, ymax, 1800)
     
     fig, ax = plt.subplots()
 
@@ -75,17 +75,17 @@ for filename in filenames:
     # Set y-tickets to be 2 decimal places
     # http://stackoverflow.com/questions/12608788/changing-the-tick-frequency-on-x-or-y-axis-in-matplotlib
     # ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.2f'))
-    label_font = 8
-    ax.set_xlabel('Mesh Sizes', fontsize=10)
+    label_font = 12
+    ax.set_xlabel('Mesh Sizes', fontsize=label_font+1)
     
     # ylabel = filename.replace('_', ' ').replace('.csv', '').title()
 
-    ax.set_ylabel('Time (msec)', fontsize=10)
+    ax.set_ylabel('Time (msec)', fontsize=label_font+1)
     labels = []
-    for i in df.Mesh_Sizes:
-        labels.append(i)
-    ax.xaxis.set_ticks(df.Mesh_Sizes)
-    ax.set_xticklabels(df.Mesh_Sizes)
+    # for i in df.Mesh_Sizes:
+    #     labels.append(i)
+    # ax.xaxis.set_ticks(df.Mesh_Sizes)
+    # ax.set_xticklabels(df.Mesh_Sizes)
     
     for tick in ax.xaxis.get_major_ticks():
         tick.label.set_fontsize(label_font)
@@ -110,7 +110,7 @@ for filename in filenames:
     handles,labels = ax.get_legend_handles_labels()
     # handles.sort()
     # labels.sort()
-    legend = ax.legend(handles, labels, loc='upper left', shadow=True, fontsize=10)
+    legend = ax.legend(handles, labels, loc='upper left', shadow=True, fontsize=label_font)
 
     # Following lines are used to drop major and minor tickes and lines
     # ax.grid(which='minor', alpha=0.2)                                                
