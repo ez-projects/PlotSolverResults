f98399118ee77e3821b91b59bb382e8744818961
En Zhou <zhouen.nathan@gmail.com>
2017-06-05 04:39:32 +0200

    Added Python3 support

--- plot_total_time_line.py
+++ plot_total_time_line.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import numpy as np
 import pandas as pd
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
@@ -15,14 +15,11 @@ from cycler import cycler
 from constants import STYLES as styles
 from constants import SOLVERS as solvers
 
-from matplotlib.lines import Line2D
-markers = []
-for m in Line2D.markers:
-    try:
-        if len(m) == 1 and m != ' ':
-            markers.append(m)
-    except TypeError:
-        pass
+import itertools
+markers = itertools.cycle(('^', 'o', '>', 'v',  'H', '<', 'd', 's', 'p','3', 'D',  'x','+', '|', '.',  '*'))
+# pudb.set_trace()
+# for n in y:
+#     plt.plot(x,n, marker = markers.next(), linestyle='')
 
 mypath = os.path.dirname(os.path.realpath(__file__))
 files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
@@ -34,7 +31,7 @@ else:
     filenames = sys.argv[1:]
 
 for filename in filenames:
-    print "Start plotting: {}".format(filename)
+    print("Start plotting: {}".format(filename))
 
     df = pd.read_csv(filename)
     df.head()
@@ -73,7 +70,7 @@ for filename in filenames:
     # Y-Axis is the total solve time in hours, i.e. from 0 to 100
     ymax = 100
     ymajor_ticks = np.arange(0, ymax, 10)
-    yminor_ticks = np.arange(0, ymax, 2)
+    yminor_ticks = np.arange(0, ymax, 5)
     
     fig, ax = plt.subplots()
 
@@ -114,7 +111,7 @@ for filename in filenames:
     ax.set_prop_cycle(cycler('color', ['r', 'b', 'c', 'y', 'g', 'm', 'k']) )
     for key, value in df.iteritems():
         if key != 'Mesh_Sizes':
-            plt.plot(x, df[key], marker=markers[i], label=key, markersize=10.0, linewidth=2.0)
+            plt.plot(x, df[key], marker=next(markers), label=key, markersize=10.0, linewidth=2.0)
             i += 1
     
     # Order the lengeds 
@@ -134,9 +131,9 @@ for filename in filenames:
     plt.tight_layout()
     path = './plots/'
     plot_name = filename.replace('.csv', '_line.eps').replace('raw_data/', '')
-    plt.savefig(path + plot_name, format='eps')
-    print plot_name
+    # plt.savefig(path + plot_name, format='eps')
+    print(plot_name)
     plot_name = filename.replace('.csv', '_line.png').replace('raw_data/', '')
-    plt.savefig(path + plot_name, format='png')
-    print plot_name
+    # plt.savefig(path + plot_name, format='png')
+    print(plot_name)
     plt.show()
